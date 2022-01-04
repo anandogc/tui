508f9ca5c295cb4ae2a08c52806a3ac6519f668b
Anando Gopal Chatterjee <anandogc@gmail.com>
2022-01-04 16:20:52 +0530

    save function added

--- main.py
+++ main.py
@@ -3,20 +3,31 @@ from PyQt5.QtWidgets import QDialog, QMainWindow, QApplication
 from TarangGUI import *
 
 class MyForm(QMainWindow):
-	def __init__(self):
-		super().__init__()
-		self.ui = Ui_MainWindow()
-		self.ui.setupUi(self)
-		self.ui.help_dropdown.activated.connect(self.disp_help)
-		print("MyForm")
+  def __init__(self):
+    super().__init__()
+    self.ui = Ui_MainWindow()
+    self.ui.setupUi(self)
+    self.ui.help_dropdown.activated.connect(self.disp_help)
+    self.ui.actionSave.triggered.connect(self.save)
 
-	def disp_help(self):
-		print(self.ui.help_dropdown.currentIndex())
-		self.ui.help.currentIndex = self.ui.help_dropdown.currentIndex()
+  def disp_help(self):
+    self.ui.help.setCurrentIndex(self.ui.help_dropdown.currentIndex())
+
+  def save(self):
+    print(rf"""
+{{
+   "program": {{
+      "kind": "{self.ui.program_kind.currentText()}"
+      "basis_type": "{self.ui.program_basis_type.currentText()}"
+   }}
+
+}}
+      """)
+      
 
 
 if __name__=="__main__":
-	app = QApplication(sys.argv)
-	w = MyForm()
-	w.show()
-	sys.exit(app.exec_())
+  app = QApplication(sys.argv)
+  w = MyForm()
+  w.show()
+  sys.exit(app.exec_())
